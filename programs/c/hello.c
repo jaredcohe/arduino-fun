4a26fc3e7e12064bb2c91b0ab218aba73fe1ecf6
Jared Cohen <jaredcohe@gmail.com>
2015-12-07 21:01:46 -0500

    stuff

--- programs/c/hello.c
+++ programs/c/hello.c
@@ -63,3 +63,34 @@ int main() {
     p = foo(2, 3);
     printf("%i", p);
 }
+
+// global variable, dont need extern line if in same file
+int global_i;
+void foo() {
+    extern int global_i;
+}
+
+// class v no class
+int x1, x2, x3, y1, y2, y3;
+x1 = 0; x2 = 0; ...
+plot (x1, y1); ...
+
+// if had a class called point
+point p1, p2, p3
+p1 = new point(0,0);
+p1.plot(); ...
+
+// another class example
+class X {
+    public:
+        int m;
+        int mf(inv v) { int old = m; m = v; return old; }
+}
+
+X var;
+var.m = 7;
+int x = var.mf(9);
+
+
+
+
